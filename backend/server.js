ee4f379c227654b115b25928e77dbbcf96a53b1f
AjithMDev <ajithajith33398@gmail.com>
2026-02-13 12:07:06 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("GMB AI Backend Running");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
